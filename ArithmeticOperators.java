3dcf55e5445b0cb07a40e5bd2b3aebd39ba825f9
Preethi KS <preethiks1998@gmail.com>
2022-10-10 12:22:36 +0530

    first

---
+++ ArithmeticOperators.java
@@ -0,0 +1,15 @@
+class ArithmeticOperators
+{
+public static void main(String[]args)
+{
+	int x=10;
+	int y=20;
+	int sum=x=y;
+	int diff=x-y;
+System.out.println("sum="+sum);
+System.out.println("differnce is " + diff);
+System.out.println(y*5);
+System.out.println(30/3);
+System.out.println(30%3);
+}
+}
